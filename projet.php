2633328c04060b4944dd884761b56e4bc2f5ae91
Perus Maxence <122904079+PerusMaxence@users.noreply.github.com>
2023-08-16 22:43:45 +0200

    Add files via upload

---
+++ projet.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <title>Projets - ATLAZ</title>
+    <link rel="shortcut icon" href="images/logoatlaz2.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
+</style>
+
+<body>
+    <header>
+        <?php include('header.php'); ?>
+    </header>
+    <div id="projet-container-1">
+        <div id="projet-container-titre">
+            <p id="Grand-Titre" style="color: white; text-align: center;">Mes travaux professionnel.</p>
+            <p id="Commentaire" style="font-size: 15px;">HTML/CSS/PHP/SQL/PYTHON.</p>
+        </div>
+    </div>
+    <div id="grande-separation-bleue"></div>
+    <br>
+    <div id="projet-container-btn">
+        <a href="html.php" style="text-decoration: none;">
+            <p style="text-align: center;"><i class="fa-solid fa-certificate fa-2xl" style="color: #000000;"></i></p>
+            <div id="ligne-horizontal" style="border: solid 1px black; width: 50px; margin-top: 10px; margin-bottom: 10px;"></div>
+            <p style="color: black; text-align: center; margin: 10px;">HTML</p>
+        </a>
+    </div>
+</body>
+
+</html>
---
+++ scolarite.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <title>Accueil - ATLAZ</title>
+    <link rel="shortcut icon" href="images/logoatlaz2.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
+</style>
+
+<body>
+    <header>
+        <?php include('header.php'); ?>
+    </header>
+    
+</body>
+
+</html>
